c31ffc7e5c7dad8059931d049f3138a767539f3b
Jiří Kubíček <jiri.kubicek@kraxnet.cz>
2014-11-05 01:22:42 +0100

    Přechod na češtinu

--- downloader.rb
+++ downloader.rb
@@ -2,9 +2,9 @@ require 'open-uri'
 
 date = Dir.glob("./records/*.doc").sort.last.to_s.match(/(\d+)/)[1]
 puts "Last #{date}"
-Date.parse(date).upto(Date.today).collect{|d| d.strftime("%Y%m%d")}[2..-1].each{ |date|
+Date.parse(date).upto(Date.today).collect{|d| d.strftime("%Y%m%d")}[1..-1].each{ |date|
   puts date
-  data = open("http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-//EP//NONSGML+PV+#{date}+RES-RCV+DOC+WORD+V0//EN&language=EN").read
+  data = open("http://www.europarl.europa.eu/sides/getDoc.do?pubRef=-//EP//NONSGML+PV+#{date}+RES-RCV+DOC+WORD+V0//CS&language=CS").read
   if data.match(/<td class="reference" align="left">Application error<\/td>/)
     puts "404"
   else
